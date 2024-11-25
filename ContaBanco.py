aa8349d1b51ceb5593e2a8f893d4712369e0c1bf
LoriJr <jrsilva.lorivas@gmail.com>
2024-11-25 13:51:06 -0300

    add método privado linha 10

--- ContaBanco.py
+++ ContaBanco.py
@@ -7,13 +7,22 @@ class ContaCorrente:
         self.agencia = agencia
         self.numero_conta = numero_conta
 
+    def _limite_conta(self): #métodos com o _ no início significam que são métodos privados por convenção
+        self.limite = -1000
+        return self.limite
+
     def sacar(self, valor_saque):
-        self.saldo -= valor_saque
-        print(f"Saque realizado, saldo: {self.consultar_saldo()}")
+        if self.saldo - valor_saque < self._limite_conta():#aqui está fazendo referência ao método privado _limite_conta
+            print("você não tem saldo suficiente para sacar esse valor")
+        else:
+            self.saldo -= valor_saque
+            print(f"Saque realizado, saldo: {self.consultar_saldo()}")
 
     def depositar(self, valor_deposito):
         self.saldo += valor_deposito 
         print(f"Depósito realizado, saldo: {self.consultar_saldo()}")
+
+   
 
     def consultar_saldo(self):
         print(f'Seu saldo é de R$ {self.saldo}')
