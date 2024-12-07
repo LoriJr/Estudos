fac87d88a2372edb69f443934b595942c92ee73e
LoriJr <jrsilva.lorivas@gmail.com>
2024-12-07 00:11:38 -0300

    add funcao CartaoCredito

--- ContaBanco.py
+++ ContaBanco.py
@@ -10,31 +10,32 @@ class ContaCorrente:
         return horario_BR.strftime('%d/%m/%Y %H:%M:%S')
 
     def __init__(self, nome, cpf, agencia, numero_conta):
-        self.nome = nome
-        self.cpf = cpf
-        self.saldo = 0
-        self.limite = None
-        self.agencia = agencia
-        self.numero_conta = numero_conta
-        self.transacoes = []
+        self._nome = nome
+        self._cpf = cpf
+        self._saldo = 0
+        self._limite = None
+        self._agencia = agencia
+        self._numero_conta = numero_conta
+        self._transacoes = []
+        self.cartoes = []
 
     def _limite_conta(self): #métodos com o _ no início significam que são métodos privados por convenção, e deve ser usado somente dentro da classe 
-        self.limite = -1000
-        return self.limite
+        self._limite = -1000
+        return self._limite
 
     def sacar(self, valor_saque):
-        if self.saldo - valor_saque < self._limite_conta():#aqui está fazendo referência ao método privado _limite_conta
+        if self._saldo - valor_saque < self._limite_conta():#aqui está fazendo referência ao método privado _limite_conta
             print("você não tem saldo suficiente para sacar esse valor")
         else:
-            self.saldo -= valor_saque
+            self._saldo -= valor_saque
             print(f"Saque realizado, saldo: {self.consultar_saldo()}")
 
     def depositar(self, valor_deposito):
-        self.saldo += valor_deposito 
-        self.transacoes.append((valor_deposito, self.saldo, ContaCorrente._data_hora()))
+        self._saldo += valor_deposito 
+        self._transacoes.append((valor_deposito, self._saldo, ContaCorrente._data_hora()))
         
     def consultar_saldo(self):
-        print(f'Seu saldo é de R$ {self.saldo}')
+        print(f'Seu saldo é de R$ {self._saldo}')
 
     def consultar_limite_cheque_especial(self):
         print("Seu limite de cheque especial é de {:,.2f}".format(self._limite_conta()))
@@ -42,16 +43,24 @@ class ContaCorrente:
     def consultar_historico_transacoes(self):
         print("Histórico de Transações")
         print("Valor, Saldo, Data e Hora")
-        for transacao in self.transacoes:
+        for transacao in self._transacoes:
             print(transacao)
 
     def transferir(self, valor, conta_destino):
-        self.saldo -= valor
-        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
+        self._saldo -= valor
+        self._transacoes.append((valor, self._saldo, ContaCorrente._data_hora()))
         conta_destino.saldo += valor
         conta_destino.transacoes.append((valor, conta_destino.saldo, ContaCorrente._data_hora()))
 
-        
+    def CartaoCredito():
+
+        def __init__(self, titular, conta_corrente):
+            self.numero= None
+            self.titular = titular
+            self.validade  = None 
+            self.cod_seguranca = None
+            self.limite = None
+            self.conta-corrente = conta_corrente 
 
 conta_lou = ContaCorrente("Lourival", "123.456.789-10", 1234, 304678)
 conta_lais = ContaCorrente('Lais', '121.950.656-10', 222, 123456)
