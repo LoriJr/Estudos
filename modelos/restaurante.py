35387ce5845a910251a823336d2e148bb7d0bd08
LoriJr <jrsilva.lorivas@gmail.com>
2024-12-14 00:12:11 -0300

    property e métodos da classe

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,20 +2,50 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        '''
+        Método construtor, nele contém os atributos da classe, e também uma lista contendo os mesmos atributos
+        '''
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
         
-    def __str__(self):
-        return self.nome, self.categoria
+    def __str__(self): 
+        '''
+        método especial para alterar a forma de impressão dos itens do objeto em String
+        nesse caso só está como exemplo, pois o método listar_restaurante já está com a formatação do abjeto
+        '''
+        return self._nome, self._categoria
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        '''
+        Este é um método da classe, onde no seu argumento por convenção, coloca-se um cls.
+        no for restaurante in cls.restaurantes é onde ele faz a referência para a classe.
+        a saída do método são os prints, já formatados.
+        '''
+        mensagem = (f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status')
+        print(mensagem)
+        print('-'* len(mensagem))
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+        
+    @property
+    def ativo(self):
+        '''
+        Propriedade que altera a impressão do atributo ativo, conforme está no return
+        '''
+        return '☒' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        '''
+        método que altera o estado do ativo para True, já que o default é false
+        '''
+        self._ativo = not self._ativo
     
     
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
